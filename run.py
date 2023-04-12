51c47ff5213cdbd0997ed42074cb4b85bb7b6885
mristimaki <rosanna.ristimaki@gmail.com>
2023-04-12 17:50:56 +0000

    Initial commit, add start game function

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+def start_game():
+    """
+    Starting the game with introduction and rules.
+    Setting the size of game board and the number of ships.
+    Lets user put in name before start.
+    """
+
+    board_size = 5
+    num_ships = 5
+    print("-+" * 20)
+    print("Welcome to Sea Battle! \n")
+    print(f"Board size: {board_size}. Number of ships: {num_ships}")
+    print("Goal: Sink all of your opponent's ships. \n")
+    print("Rules: ")
+    print("- Player starts with guessing column and row.")
+    print("- The column and row guess should be between 0-4")
+    print("- Top left corner is row: 0, col: 0.")
+    print("- You will be informed if you got a 'Hit' or 'Miss'")
+    print("- Computer turn.")
+    print("- New turn.")
+    print("-+" * 20)
+    user_name = input("Please enter your name before start: ")
+    
+
+start_game()
